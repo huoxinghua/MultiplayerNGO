9c46643536c2dbf9a74b79e3303e7ebc96a11968
huoxinghua <huoxinghuacn@gmail.com>
2025-09-22 21:32:08 -0600

    take sample roughly work

---
+++ Assets/Personal/XHGamePlay/Scripts/MVCItems/SampleJar/SampleData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SampleData
+{
+    public float researchValue;
+    public int moneyValue;
+
+    public SampleData(float research, int money)
+    {
+        researchValue = research;
+        moneyValue = money;
+    }
+
+}
--- Assets/Personal/XHGamePlay/Scripts/MVCItems/SampleJar/SampleJarController.cs
+++ Assets/Personal/XHGamePlay/Scripts/MVCItems/SampleJar/SampleJarController.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
-public class SampleJarController : MonoBehaviour,IHeldItem, IInteractable
+public class SampleJarController : MonoBehaviour, IHeldItem, IInteractable
 {
     private SampleJarModel model;
     private IView view;
+    private Dictionary<string, List<SampleData>> samplesContainer = new Dictionary<string, List<SampleData>>();
+    private bool isGetSample = false;
     private void Awake()
     {
         model = new SampleJarModel();
@@ -13,16 +17,49 @@ public class SampleJarController : MonoBehaviour,IHeldItem, IInteractable
     {
         if (!model.HasOwner || !model.IsInHand) return;
 
-        Transform dropPoint = model.Owner.transform.GetChild(3); 
+        Transform dropPoint = model.Owner.transform.GetChild(3);
         view.MoveToPosition(dropPoint.position);
         view.DestroyHeldVisual();
         view.SetVisible(true);
         view.SetPhysicsEnabled(true);
-   
+
 
         model.ClearOwner();
     }
+    public void Use()
+    {
+        view.SetPhysicsEnabled(true);
+        if (!model.HasOwner || !model.IsInHand) return;
+        Debug.Log("use Sample jar");
+        model.Toggle();
 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(" sample jar trigger sth");
+        var sampleObj = other.GetComponent<ISampleable>();
+        if (sampleObj != null)
+        {
+            Debug.Log(" sample jar trigger sample");
+            var currentSample = sampleObj.GetSample();
+            SaveSample(currentSample);
+            isGetSample = true;
+            Destroy(other.gameObject, 2f);
+        }
+    }
+  
+    public void SaveSample(SampleSO value)
+    {
+        Debug.Log("Save sample:"+ value.name);
+        SampleData data = new SampleData(value.GetRandomResearchValue(),
+        value.GetRandomMoneyValue());
+        if (!samplesContainer.ContainsKey(value.sampleType))
+        {
+            samplesContainer[value.sampleType] = new List<SampleData>();
+        }
+        samplesContainer[value.sampleType].Add(data);
+        Debug.Log("sample container:"+ samplesContainer.Count);
+    }
     public void OnInteract(GameObject interactingPlayer)
     {
         var inventory = interactingPlayer.GetComponent<Inventory>();
@@ -44,7 +81,7 @@ public class SampleJarController : MonoBehaviour,IHeldItem, IInteractable
 
     public void Pickup()
     {
-        view.SetVisible(false);
+        view.SetVisible(true);
         view.SetPhysicsEnabled(false);
         view.DisplayHeld(model.Owner.transform.GetChild(0).GetChild(0));
         transform.parent = model.Owner.transform.GetChild(0).GetChild(0);
@@ -60,14 +97,11 @@ public class SampleJarController : MonoBehaviour,IHeldItem, IInteractable
     public void SwapTo()
     {
         if (!model.HasOwner && view.GetCurrentVisual() != null) return;
-      
+
         view.DisplayHeld(model.Owner.transform.GetChild(0).GetChild(0));
         model.InHand(true);
     }
 
-    public void Use()
-    {
-        throw new System.NotImplementedException();
-    }
+
 
 }
