a80d52823fd55a8e0364bcff3c3a1bfe22079b13
Altorook <nathanberry403@gmail.com>
2025-09-25 21:21:17 -0600

    Maybe Fixed Brute hearing bug

--- Assets/A_Nathan/Scripts/NPC/Violent/Brute/BruteHearing.cs
+++ Assets/A_Nathan/Scripts/NPC/Violent/Brute/BruteHearing.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BruteHearing : MonoBehaviour
@@ -14,8 +16,8 @@ public class BruteHearing : MonoBehaviour
     private bool _isOnHearingCooldown;
     private int _timesAlerted = 0;
     private int _maxTimesAlerted = 3;
-
-    void OnEnable()
+    private HashSet<PlayerMovement> _subscribedPlayers = new();
+    /*void OnEnable()
     {
         PlayerMovement.OnPlayerAdded += HandlePlayerAdded;
         PlayerMovement.OnPlayerRemoved += HandlePlayerRemoved;
@@ -27,34 +29,51 @@ public class BruteHearing : MonoBehaviour
             player.OnFalling += PlayerLanded;
             player.OnRunning += PlayerRunning;
         }
+    }*/
+    void OnEnable()
+    {
+        PlayerMovement.OnPlayerAdded += HandlePlayerAdded;
+        PlayerMovement.OnPlayerRemoved += HandlePlayerRemoved;
+
+        foreach (var player in PlayerMovement.AllPlayers)
+        {
+            HandlePlayerAdded(player); // centralize logic
+        }
     }
 
     void OnDisable()
     {
         PlayerMovement.OnPlayerAdded -= HandlePlayerAdded;
         PlayerMovement.OnPlayerRemoved -= HandlePlayerRemoved;
-        //unsub from all existing players
-        foreach (var player in PlayerMovement.AllPlayers)
+
+        foreach (var player in _subscribedPlayers.ToList()) // to avoid modification during enumeration
         {
-            if (player != null)
-                player.OnWalking -= PlayerWalking;
-            player.OnLand -= PlayerLanded;
-            player.OnRunning -= PlayerRunning;
+            HandlePlayerRemoved(player);
         }
+
+        _subscribedPlayers.Clear();
     }
     //player joined game, add to list
     void HandlePlayerAdded(PlayerMovement player)
     {
+        if (_subscribedPlayers.Contains(player)) return;
+
         player.OnWalking += PlayerWalking;
         player.OnLand += PlayerLanded;
         player.OnRunning += PlayerRunning;
+
+        _subscribedPlayers.Add(player);
     }
-    //player joined game, add to list
+
     void HandlePlayerRemoved(PlayerMovement player)
     {
+        if (!_subscribedPlayers.Contains(player)) return;
+
         player.OnWalking -= PlayerWalking;
         player.OnLand -= PlayerLanded;
         player.OnRunning -= PlayerRunning;
+
+        _subscribedPlayers.Remove(player);
     }
 
     void PlayerWalking(GameObject player)
