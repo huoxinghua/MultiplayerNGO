986fa8b59b9f914461b4e2d28d9f165b7fec0a8b
Nathan <nathanberry403@gmail.com>
2025-10-05 18:39:45 -0600

    PlayerStateMachine scripts made and started

---
+++ Assets/_Project/Code/Gameplay/Player/PlayerStateMachine/PlayerBaseState.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerBaseState : BaseState
+{
+    protected PlayerStateMachine stateController;
+
+    public PlayerBaseState(PlayerStateMachine stateController)
+    {
+        this.stateController = stateController;
+
+    }
+    public override void OnEnter()
+    {
+        
+    }
+    public override void OnExit()
+    {
+        
+    }
+
+    public override void StateFixedUpdate()
+    {
+        
+    }
+
+    public override void StateUpdate()
+    {
+        
+    }
+    
+}
