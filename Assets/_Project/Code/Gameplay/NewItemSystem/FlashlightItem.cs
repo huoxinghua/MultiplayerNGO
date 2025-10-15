daf6dfaeb22ef7a715ef3140f13186a849bd83c6
Altorook <nathanberry403@gmail.com>
2025-10-14 23:05:58 -0600

    Started flashlight

---
+++ Assets/_Project/Code/Gameplay/NewItemSystem/FlashlightItem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FlashlightItem : MonoBehaviour, IInventoryItem
+{
+
+    public void PickupItem()
+    {
+
+    }
+    public void DropItem()
+    {
+
+    }
+    public void UseItem()
+    {
+    }
+    public void UnequipItem()
+    {
+    }
+    public void EquipItem()
+    {
+    }
+    public string GetItemName()
+    {
+        //replace with SO stuff
+        return "Flash";
+    }
+    public bool IsPocketSize()
+    {
+        //replace with SO stuff
+        return true;
+    }
+
+}
