cff49e06f0591c17c3cc5f76e5aa74851cfb23cc
KyleTheMan <KyleHasWork@gmail.com>
2025-10-16 16:55:58 -0600

    Finished light manager
    -Lights can be turned off and on
    -Light flickering can be toggled on and off

--- Assets/_Project/Code/Scripts/LightFunction/LightManager.cs
+++ Assets/_Project/Code/Scripts/LightFunction/LightManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class CeilingLight : MonoBehaviour
 {
@@ -7,7 +8,11 @@ public class CeilingLight : MonoBehaviour
     private Renderer _objRenderer;
     private Light _spotLightSource;
     private Light _pointLightSource;
-    
+    private bool _isFlickering;
+    public float _minIntensity = 0.1f;
+    public float _maxIntensity = 2f;
+    public float _originalIntensity = 0.6f;
+
     void Start()
     {
         _objRenderer = GetComponent<Renderer>();
@@ -17,14 +22,61 @@ public class CeilingLight : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            _objRenderer.material = _newMaterial;
+            TurnLightOn();
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            _pointLightSource.enabled = !_pointLightSource.enabled;
-            _spotLightSource.enabled = _pointLightSource.enabled;
+            TurnLightOff();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartFlickering();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StopFlickering();
         }
     }
+
+    private void TurnLightOn()
+    {
+        _pointLightSource.enabled = true;
+        _spotLightSource.enabled = true;
+        _objRenderer.material = _oldMaterial;
+    }
+
+    private void TurnLightOff()
+    {
+        _pointLightSource.enabled = false;
+        _spotLightSource.enabled = false;
+        _objRenderer.material = _newMaterial;
+    }
+
+    private void StartFlickering()
+    {
+        _isFlickering = true;
+        StartCoroutine(LightFlicker());
+    }
+    private void StopFlickering()
+    {
+        _isFlickering = false;
+        StopCoroutine(LightFlicker());
+        _spotLightSource.intensity = _originalIntensity;
+        _pointLightSource.intensity = _originalIntensity;
+    }
+
+
+    IEnumerator LightFlicker()
+    {
+        while (_isFlickering == true)
+        {
+            float randomIntensity = Random.RandomRange(_minIntensity, _maxIntensity);
+            _spotLightSource.intensity = randomIntensity;
+            _pointLightSource.intensity = randomIntensity;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
 }
