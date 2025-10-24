66f20f1d44898281b1111402134ea4b949c7cf96
Altorook <nathanberry403@gmail.com>
2025-10-23 22:38:44 -0600

    Outline works

--- Assets/_Project/Code/Gameplay/Interactables/ShopMap.cs
+++ Assets/_Project/Code/Gameplay/Interactables/ShopMap.cs
@@ -1,12 +1,24 @@
 using _Project.Code.Gameplay.Interactables;
 using _Project.Code.Gameplay.Player.PlayerStateMachine;
+using QuickOutline.Scripts;
 using UnityEngine;
 
+[RequireComponent(typeof(Outline))]
 public class ShopMap : MonoBehaviour , IInteractable
 {
     [SerializeField] private Camera _shopCamera;
     [SerializeField] private Camera _playerCamera;
     [SerializeField] private PlayerStateMachine _playerStateMachine;
+    protected Outline OutlineEffect;
+    public void Awake()
+    {
+        OutlineEffect = GetComponent<Outline>();
+        if (OutlineEffect != null)
+        {
+            OutlineEffect.OutlineMode = Outline.Mode.OutlineHidden;
+            OutlineEffect.OutlineWidth = 0;
+        }
+    }
     public void OnInteract(GameObject interactingPlayer)
     {
         if (interactingPlayer.GetComponent<PlayerStateMachine>() != null)
@@ -16,6 +28,7 @@ public class ShopMap : MonoBehaviour , IInteractable
             _shopCamera.enabled = true;
             _playerCamera = interactingPlayer.GetComponentInChildren<Camera>();
             _playerCamera.enabled = false;
+            HandleHover(false);
         }
     }
     public void OnClose()
@@ -23,10 +36,25 @@ public class ShopMap : MonoBehaviour , IInteractable
         _playerCamera.enabled = true;
         _shopCamera.enabled = false;
         _playerStateMachine.HandleOpenMenu(false);
+        _playerStateMachine = null;
+        _playerCamera = null;
     }
 
     public void HandleHover(bool isHovering)
     {
-
+        if (OutlineEffect != null)
+        {
+            if (_playerCamera != null) { OutlineEffect.OutlineMode = Outline.Mode.OutlineHidden; return; }
+            if (isHovering)
+            {
+                OutlineEffect.OutlineMode = Outline.Mode.OutlineVisible;
+                OutlineEffect.OutlineWidth = 2;
+            }
+            else
+            {
+                OutlineEffect.OutlineMode = Outline.Mode.OutlineHidden;
+                OutlineEffect.OutlineWidth = 0;
+            }
+        }
     }
 }
