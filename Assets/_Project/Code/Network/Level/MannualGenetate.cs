6f8031a558c10c77397b87a0efc096f0c31bd234
Xinghua <huoxinghuacn@gmail.com>
2025-11-09 13:05:13 -0700

    clean up levelsync：prefab  DungeonGenerator_NW_00

--- Assets/_Project/Code/Network/Level/MannualGenetate.cs
+++ Assets/_Project/Code/Network/Level/MannualGenetate.cs
@@ -2,19 +2,18 @@ using _Project.Code.Gameplay.Interactables.NoUse;
 using DunGen;
 using Unity.Netcode;
 using UnityEngine;
+
 namespace _Project.Code.Network.Level
 {
     public class MannualGenetate : NetworkBehaviour
     {
         private RuntimeDungeon generator;
-
         private void Start()
         {
             generator = GetComponent<RuntimeDungeon>();
-            
         }
-   
-        
+
+
         private NetworkVariable<int> syncedSeed = new NetworkVariable<int>(
             default,
             NetworkVariableReadPermission.Everyone,
@@ -25,29 +24,22 @@ namespace _Project.Code.Network.Level
         {
             if (generator == null)
                 generator = GetComponent<RuntimeDungeon>();
-      
+
 
             if (IsServer)
             {
-            
                 int seed = Random.Range(0, int.MaxValue);
                 syncedSeed.Value = seed;
-                Debug.Log($"[Server] Chosen seed = {seed}");
-
                 generator.Generator.ShouldRandomizeSeed = false;
                 generator.Generator.Seed = seed;
-                /*generator.ShouldRandomizeSeed = false;
-                generator.Seed = seed;*/
                 generator.Generate();
                 OnDungeonGenerated(generator);
-
             }
             else
             {
-
                 syncedSeed.OnValueChanged += OnSeedReceived;
 
-      
+
                 if (syncedSeed.Value != default)
                 {
                     OnSeedReceived(default, syncedSeed.Value);
@@ -57,16 +49,12 @@ namespace _Project.Code.Network.Level
 
         private void OnSeedReceived(int oldSeed, int newSeed)
         {
-            Debug.Log($"[Client] Received dungeon seed = {newSeed}");
-
-
             ClearOldDungeonTiles();
 
             generator.Generator.ShouldRandomizeSeed = false;
-            generator.Generator.Seed = newSeed ;
+            generator.Generator.Seed = newSeed;
             generator.Generate();
-
-            Debug.Log("[Client] Dungeon generated from shared seed.");
+            
             foreach (var door in Object.FindObjectsByType<SwingDoors>(FindObjectsSortMode.None))
             {
                 var netObj = door.GetComponent<NetworkObject>();
@@ -75,7 +63,6 @@ namespace _Project.Code.Network.Level
                     Destroy(door.gameObject);
                 }
             }
-
         }
 
         private void ClearOldDungeonTiles()
@@ -84,6 +71,7 @@ namespace _Project.Code.Network.Level
             foreach (var t in oldTiles)
                 Destroy(t);
         }
+
         void OnDungeonGenerated(RuntimeDungeon dungeon)
         {
             foreach (var door in Object.FindObjectsByType<SwingDoors>(FindObjectsSortMode.None))
