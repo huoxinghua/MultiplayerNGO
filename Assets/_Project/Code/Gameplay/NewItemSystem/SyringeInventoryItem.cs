77627593d06f97ea2d83e22f0d9a87ba257c64d4
KyleTheMan <KyleHasWork@gmail.com>
2025-11-12 21:11:33 -0700

    Syringe be funny

--- Assets/_Project/Code/Gameplay/NewItemSystem/SyringeInventoryItem.cs
+++ Assets/_Project/Code/Gameplay/NewItemSystem/SyringeInventoryItem.cs
@@ -8,6 +8,63 @@ namespace _Project.Code.Gameplay.NewItemSystem
 {
     public class SyringeInventoryItem : BaseInventoryItem
     {
-        
+        NetworkVariable<bool> IsUsed = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone,
+            NetworkVariableWritePermission.Server);
+
+        private SyringeItemSO _syringeItemSo;
+
+        #region Setup + Update
+
+        private void Awake()
+        {
+            if (_itemSO is SyringeItemSO syringeItemSO)
+            {
+                _syringeItemSo = syringeItemSO;
+            }
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            Debug.Log("CustomNetworkSpawn called!");
+            // Now add flashlight-specific network setup
+            CustomNetworkSpawn();
+        }
+
+        private void Update()
+        {
+            if (!IsOwner) return; // only the owning player updates
+            UpdateHeldPosition();
+        }
+
+        #endregion
+
+        #region UseLogic
+
+        public override void UseItem()
+        {
+            base.UseItem();
+            if (IsUsed.Value) return;
+            if (IsOwner)
+            {
+                InjectSyringe();
+            }
+        }
+
+        private void InjectSyringe()
+        {
+            /*_syringeItemSo.EffectDuration;
+            _syringeItemSo.SpeedBoostAmount;*/
+            Debug.Log("InjectSyringe");
+            RequestChangeIsUsedServerRpc();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void RequestChangeIsUsedServerRpc()
+        {
+            IsUsed.Value = true;
+        }
+
+        #endregion
     }
 }
