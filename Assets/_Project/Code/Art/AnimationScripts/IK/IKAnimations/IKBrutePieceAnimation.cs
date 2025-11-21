eb68dd5bec3f3cc9aa062f3cdce049822cb82a3e
Smooztime <chanakan.zoom@gmail.com>
2025-11-21 11:02:06 -0700

    Setup all Brute parts for Held
    To merge this, needs change something in HeldBaseVisual

--- Assets/_Project/Code/Art/AnimationScripts/IK/IKAnimations/IKBrutePieceAnimation.cs
+++ Assets/_Project/Code/Art/AnimationScripts/IK/IKAnimations/IKBrutePieceAnimation.cs
@@ -1,7 +1,7 @@
 using _Project.Code.Art.AnimationScripts.IK;
 using UnityEngine;
 
-public class IKBruteHeadAnimation : IKItemAnimation
+public class IKBrutePieceAnimation : IKItemAnimation
 {
     public override void PlayIKInteractLocal(bool isFPS)
     {
