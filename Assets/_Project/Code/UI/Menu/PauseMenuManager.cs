ef2b4e080183d3fb6dcef7b7cf565b488339f9e2
huoxinghua <huoxinghuacn@gmail.com>
2025-12-08 15:52:52 -0700

    pause menu work now ; input P cause VM

--- Assets/_Project/Code/UI/Menu/PauseMenuManager.cs
+++ Assets/_Project/Code/UI/Menu/PauseMenuManager.cs
@@ -1,69 +1,88 @@
 using System;
+using _Project.Code.Core.Patterns;
 using _Project.Code.Gameplay.Player.MiscPlayer;
+using _Project.Code.Utilities.EventBus;
 using UnityEngine;
 
-namespace _Project.Code.UI
+namespace _Project.Code.UI.Menu
 {
-public class PauseMenuManager : MonoBehaviour
-{
-    private GameObject pauseMenuUI;
-    [SerializeField] private PlayerInputManager playerInput;
-    [SerializeField] private GameObject pauseMenuPrefab;
-    private bool _isPaused = false;
-
-    private void Start()
+    public class PauseMenuManager : MonoBehaviour
     {
-        playerInput.OnPauseOpen+= TogglePauseMenu;
-        playerInput.OnPauseClose+= TogglePauseMenu;
-    }
+        //this code attached with player,so keep Monobehaviour
+        private GameObject pauseMenuUI;
+        [SerializeField] private PlayerInputManager playerInput;
+        [SerializeField] private GameObject pauseMenuPrefab;
+        private bool _isPaused = false;
 
-    private void OnDisable()
-    {
-        playerInput.OnPauseOpen-= TogglePauseMenu;
-        playerInput.OnPauseClose-= TogglePauseMenu;
-    }
-
-    public void TogglePauseMenu()
-    {
-        Debug.Log("pause menu open  ");
-        if (!_isPaused)
-            OpenPause();
-        else
-            ClosePause();
-    }
-
-    private void OpenPause()
-    {
-        _isPaused = true;
-
-      
-        if (pauseMenuUI == null)
+        private void Start()
         {
-            
-            pauseMenuUI = Instantiate(pauseMenuPrefab);
+            playerInput.OnPauseOpen+= TogglePauseMenu;
+            playerInput.OnPauseClose+= TogglePauseMenu;
+            EventBus.Instance.Subscribe<PauseMenuResumeEvent>(this,ClosePause);
         }
 
-        pauseMenuUI.SetActive(true);
+        private void OnDisable()
+        {
+            playerInput.OnPauseOpen-= TogglePauseMenu;
+            playerInput.OnPauseClose-= TogglePauseMenu;
+        }
 
-        Time.timeScale = 0f;
-        playerInput.SwitchToUIMode();
+        public void TogglePauseMenu()
+        {
+            if (!_isPaused)
+                OpenPause();
+            else
+                ClosePause();
+        }
 
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+        private void OpenPause()
+        {
+            _isPaused = true;
+
+      
+            if (pauseMenuUI == null)
+            {
+            
+                pauseMenuUI = Instantiate(pauseMenuPrefab);
+       
+         
+            }
+
+            pauseMenuUI.SetActive(true);
+            var pauseMenu = pauseMenuUI.GetComponent<PauseMenu>();
+            pauseMenu.DisplayPauseMenu();
+            Time.timeScale = 0f;
+            playerInput.SwitchToUIMode();
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        public void ClosePause()
+        {
+            Debug.Log("pause menu close  ");
+            _isPaused = false;
+
+            pauseMenuUI.SetActive(false);
+
+            Time.timeScale = 1f;
+            playerInput.SwitchToPlayerMode();
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        public void ClosePause(PauseMenuResumeEvent evt)
+        {
+            Debug.Log("pause menu close  ");
+            _isPaused = false;
+
+            pauseMenuUI.SetActive(false);
+
+            Time.timeScale = 1f;
+            playerInput.SwitchToPlayerMode();
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
-
-    private void ClosePause()
-    {
-        Debug.Log("pause menu close  ");
-        _isPaused = false;
-
-        pauseMenuUI.SetActive(false);
-
-        Time.timeScale = 1f;
-        playerInput.SwitchToPlayerMode();
-
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-}
 }
--- Assets/_Project/Code/UI/PauseMenu.cs
+++ Assets/_Project/Code/UI/PauseMenu.cs
@@ -1,3 +1,6 @@
+using System;
+using _Project.Code.Gameplay.Player.MiscPlayer;
+using _Project.Code.Utilities.EventBus;
 using UnityEngine;
 
 namespace _Project.Code.UI
@@ -8,6 +11,8 @@ namespace _Project.Code.UI
         public GameObject ResumeButton;
         public GameObject ControlsButton;
         public GameObject QuitButton;
+        public GameObject PauseTitle;
+
 
         public void Start()
         {
@@ -16,26 +21,52 @@ namespace _Project.Code.UI
 
         public void DisplayPauseMenu()
         {
+            PauseTitle.SetActive(true);
             ControlsMenu.SetActive(false);
             ResumeButton.SetActive(true);
             ControlsButton.SetActive(true);
             QuitButton.SetActive(true);
         }
 
-        /*public void DisplayControlsMenu()
+        public void DisplayControlsMenu()
         {
+            PauseTitle.SetActive(false);
             ControlsMenu.SetActive(true);
             ResumeButton.SetActive(false);
             ControlsButton.SetActive(false);
             QuitButton.SetActive(false);
         }
 
-        public void ClosePauseMenu()
+        public void OnClickResume()
         {
+            PauseTitle.SetActive(false);
             ControlsMenu.SetActive(false);
             ResumeButton.SetActive(false);
             ControlsButton.SetActive(false);
             QuitButton.SetActive(false);
-        }*/
+            EventBus.Instance.Publish(new PauseMenuResumeEvent{});
+        }
+        public void OnClickQuitGame()
+        {
+            if (Unity.Netcode.NetworkManager.Singleton != null && Unity.Netcode.NetworkManager.Singleton.IsListening)
+            {
+                Unity.Netcode.NetworkManager.Singleton.Shutdown();
+            }
+
+            Application.Quit();
+
+
+#if UNITY_EDITOR
+
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+
     }
+
+    public struct PauseMenuResumeEvent : IEvent
+    {
+        
+    }
+        
 }
